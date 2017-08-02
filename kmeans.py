f54639cf559c31448bde53a4866b40f02ff4eed8
rishabhrishu <iit2015068@iiita.ac.in>
2017-08-03 00:07:49 +0530

    working copy k-means

---
+++ kmeans.py
@@ -0,0 +1,12 @@
+import random
+k = input()
+points = []
+centroid = []
+cluster = []
+while True :
+	x = random.randint(1,200)
+	y = random.randint(1,200)
+	if (x,y) not in points:
+		points.append((x,y)
+	if len(points) ==20 :
+		break
